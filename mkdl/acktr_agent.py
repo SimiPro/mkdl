54f61baff103fd0b885176bc48b4ccf337259069
Simi <sihuber@gmx.net>
2017-12-09 08:43:33 +0100

    add acktr agent

---
+++ mkdl/acktr_agent.py
@@ -0,0 +1,47 @@
+import os, logging, gym
+from baselines import logger
+from baselines.common import set_global_seeds
+from baselines import bench
+from baselines.acktr.acktr_disc import learn
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+from baselines.acktr.policies import CnnPolicy
+
+from reinforcement import MarioEnv
+
+
+
+def train(num_timesteps, seed, num_cpu):
+    # TODO: Just fuckin ugly handle that better
+    def make_env(rank):
+        def _thunk():
+            print(rank)
+            if num_cpu == 1:
+                env = MarioEnv(num_steering_dir=11)
+            else:
+                env = MarioEnv(num_steering_dir=11, num_env=rank)
+            env.seed(seed + rank)
+            env = bench.Monitor(env, logger.get_dir() and os.path.join(logger.get_dir(), str(rank)))
+            gym.logger.setLevel(logging.WARN)
+            return env
+        return _thunk
+    set_global_seeds(seed)
+    env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+    policy_fn = CnnPolicy
+    learn(policy_fn, env, seed, total_timesteps=int(num_timesteps * 1.1), nprocs=num_cpu)
+    env.close()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
+    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument('--num-timesteps', type=int, default=int(10e6))
+    args = parser.parse_args()
+    logger.configure()
+    train(num_timesteps=args.num_timesteps, seed=args.seed, num_cpu=4)
+
+
+if __name__ == '__main__':
+    main()
