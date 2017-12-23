7a1d1d4384e5474c69f461a73836c06a9b737054
Simi <sihuber@gmx.net>
2017-12-23 11:08:33 +0100

    add new policy & be able to execute runs without learning

--- mkdl/ppo2_agent.py
+++ mkdl/ppo2_agent.py
@@ -13,7 +13,42 @@ import os.path as osp
 import tensorflow as tf
 
 from mario_env import MarioEnv
-from policy import ContCnnPolicy
+from policy import ContCnnPolicy, OurCNN
+
+def run(env_id, num_timesteps, seed, policy):
+    ncpu = multiprocessing.cpu_count()
+    if sys.platform == 'darwin': ncpu //= 2
+    config = tf.ConfigProto(allow_soft_placement=True,
+                            intra_op_parallelism_threads=ncpu,
+                            inter_op_parallelism_threads=ncpu)
+    config.gpu_options.allow_growth = True #pylint: disable=E1101
+    gym.logger.setLevel(logging.WARN)
+    tf.Session(config=config).__enter__()
+    nenvs = 1
+    def make_env(rank):
+        def env_fn():
+            print(rank)
+            if nenvs == 1:
+                env = MarioEnv(num_steering_dir=11)
+            else:
+                env = MarioEnv(num_steering_dir=11, num_env=rank)
+            env.seed(seed + rank)
+            env = bench.Monitor(env, logger.get_dir() and osp.join(logger.get_dir(), str(rank)))
+            gym.logger.setLevel(logging.WARN)
+            return env
+        return env_fn
+
+    env = SubprocVecEnv([make_env(i) for i in range(nenvs)])
+    set_global_seeds(seed)
+    env = VecFrameStack(env, 4)
+    policy = {'cont': ContCnnPolicy, 'cnn' : OurCNN, 'lstm' : LstmPolicy, 'lnlstm' : LnLstmPolicy}[policy]
+    ppo2.run_only(policy=policy, env=env, nsteps=128, nminibatches=4,
+        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
+        ent_coef=.01,
+        lr=lambda f : f * 1e-3,
+        cliprange=lambda f : f * 0.1,
+        total_timesteps=int(num_timesteps * 1.1),
+               save_interval=10)
 
 
 def train(env_id, num_timesteps, seed, policy):
@@ -25,14 +60,14 @@ def train(env_id, num_timesteps, seed, policy):
     config.gpu_options.allow_growth = True #pylint: disable=E1101
     gym.logger.setLevel(logging.WARN)
     tf.Session(config=config).__enter__()
-    nenvs = 4
+    nenvs = 8
     def make_env(rank):
         def env_fn():
             print(rank)
             if nenvs == 1:
-                env = MarioEnv(num_steering_dir=0)
+                env = MarioEnv(num_steering_dir=11)
             else:
-                env = MarioEnv(num_steering_dir=0, num_env=rank)
+                env = MarioEnv(num_steering_dir=11, num_env=rank)
             env.seed(seed + rank)
             env = bench.Monitor(env, logger.get_dir() and osp.join(logger.get_dir(), str(rank)))
             gym.logger.setLevel(logging.WARN)
@@ -42,20 +77,25 @@ def train(env_id, num_timesteps, seed, policy):
     env = SubprocVecEnv([make_env(i) for i in range(nenvs)])
     set_global_seeds(seed)
     env = VecFrameStack(env, 4)
-    policy = {'cont': ContCnnPolicy, 'cnn' : CnnPolicy, 'lstm' : LstmPolicy, 'lnlstm' : LnLstmPolicy}[policy]
-    ppo2.learn(policy=policy, env=env, nsteps=100, nminibatches=4,
-        lam=0.95, gamma=0.95, noptepochs=4, log_interval=1,
+    policy = {'cont': ContCnnPolicy, 'cnn' : OurCNN, 'lstm' : LstmPolicy, 'lnlstm' : LnLstmPolicy}[policy]
+    ppo2.learn(policy=policy, env=env, nsteps=128, nminibatches=4,
+        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
         ent_coef=.01,
-        lr=lambda f : f * 2.5e-4,
+        lr=lambda f : f * 1e-3,
         cliprange=lambda f : f * 0.1,
         total_timesteps=int(num_timesteps * 1.1),
                save_interval=10)
+
+    # noptepochs how many times learning from a batch
+    # nminibatches how many samples should we learn from. bigger if cont. space
+    # ent_coef = entropy how much caos=?
+    # lr = learning rate
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cont')
+    parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
     parser.add_argument('--num-timesteps', type=int, default=int(10e6))
     args = parser.parse_args()
     logger.configure()
@@ -63,5 +103,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
